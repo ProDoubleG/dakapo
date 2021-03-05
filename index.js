17ca5ca1a916b58d42a26863d023b99e00084b25
ProDoubleG <78391621+ProDoubleG@users.noreply.github.com>
2021-03-05 18:01:23 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+function q1to2(){
+    document.getElementById("Q1_1").style.display = "none"
+    document.getElementById("Q1_2").style.display = "block"
+}
+function q2to3(){
+    document.getElementById("Q1_2").style.display = "none"
+    document.getElementById("Q1_3").style.display = "block"
+}
+function q3to4(){
+    document.getElementById("Q1_3").style.display = "none"
+    document.getElementById("Q1_4").style.display = "block"
+}
+function q4to5(){
+    document.getElementById("Q1_4").style.display = "none"
+    document.getElementById("Q1_5").style.display = "block"
+}
+function q5to6(){
+    document.getElementById("Q1_5").style.display = "none"
+    document.getElementById("Q2_1").style.display = "block"
+}
+function q6to7(){
+    document.getElementById("Q2_1").style.display = "none"
+    document.getElementById("Q2_2").style.display = "block"
+}
+function q7to8(){
+    document.getElementById("Q2_2").style.display = "none"
+    document.getElementById("Q2_3").style.display = "block"
+}
+function q8to9(){
+    document.getElementById("Q2_3").style.display = "none"
+    document.getElementById("Q2_4").style.display = "block"
+}
+function q9to10(){
+    document.getElementById("Q2_4").style.display = "none"
+    document.getElementById("Q2_5").style.display = "block"
+}
+function q10to11(){
+    document.getElementById("Q2_5").style.display = "none"
+    document.getElementById("Q3_1").style.display = "block"
+}
+function q11to12(){
+    document.getElementById("Q3_1").style.display = "none"
+    document.getElementById("Q3_2").style.display = "block"
+}
+function q12to13(){
+    document.getElementById("Q3_2").style.display = "none"
+    document.getElementById("Q3_3").style.display = "block"
+}
+function q13to14(){
+    document.getElementById("Q3_3").style.display = "none"
+    document.getElementById("Q3_4").style.display = "block"
+}
+function q14to15(){
+    document.getElementById("Q3_4").style.display = "none"
+    document.getElementById("Q3_5").style.display = "block"
+}
+function q15to16(){
+    document.getElementById("Q3_5").style.display = "none"
+    document.getElementById("Q4_1").style.display = "block"
+}
+function q16to17(){
+    document.getElementById("Q4_1").style.display = "none"
+    document.getElementById("Q4_2").style.display = "block"
+}
+function q17to18(){
+    document.getElementById("Q4_2").style.display = "none"
+    document.getElementById("Q4_3").style.display = "block"
+}
+function q18to19(){
+    document.getElementById("Q4_3").style.display = "none"
+    document.getElementById("Q4_4").style.display = "block"
+}
+function q19to20(){
+    document.getElementById("Q4_4").style.display = "none"
+    document.getElementById("Q4_5").style.display = "block"
+}
+function show(){
+    document.getElementById("Q4_5").style.display = "none"
+    document.getElementById("result").style.display = "block"
+}
